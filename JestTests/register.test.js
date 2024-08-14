fb1c059fd683ebe233683ea66a7dedce5e0107a3
willcook118 <williamcook118@hotmail.co.uk>
2024-08-14 11:23:43 +0100

    change test to use axios against the running server rather than using mock with supertest

--- JestTests/register.test.js
+++ JestTests/register.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
+const axios = require('axios');
+const baseUrl = 'http://localhost:3000'
 
 const random = () => {
     return Math.floor(Math.random() * 10000)
@@ -8,58 +10,107 @@ const random = () => {
 describe('auth endpoints', () => {
     it('Register - valid username and password', async () => {
         const randomNumber = random()
-        const res = await request(app)
-            .post('/auth/register')
-            .send({username: 'testingUser' + randomNumber, password: 'testingpassword123!'})
-            .expect(201);
-            expect(res.statusCode).toBe(201);
+        
+        const response = await axios.post(baseUrl + '/auth/register', {
+            username: 'testUser' + randomNumber,
+            password: 'testingpassword123!'
+        })
+        expect(response.status).toBe(201)
     })
 
     it('Register - empty username with empty password', async () => {
-        const res = await request(app)
-        .post('/auth/register')
-        .send({username: '', password: ''})
 
-        expect(res.statusCode).toBe(400)
-        expect(res.body.error).toBe('Please enter a username and password')
+            const response = await axios.post(baseUrl + '/auth/register', {
+            username: '',
+            password: ''
+        }, {
+            validateStatus: null
+        })
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Please enter a username and password')
     })
 
-    it('Register - empty username with valid password', async () => {
-        const res = await request(app)
-        .post('/auth/register')
-        .send({username: '', password: 'testingpassword123!'})
-
-        expect(res.statusCode).toBe(400)
-        expect(res.body.error).toBe('Please enter a username')
+    it('Register - empty username with valid password', async() => {
+        const response = await axios.post(baseUrl + '/auth/register', {
+            username: '',
+            password: 'testingpassword123!'
+        },{
+            validateStatus: null
+        })
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Please enter a username')
     })
 
-    it('Register - valid username with empty password', async () => {
+    it('Register - valid username with empty password', async() => {
         const randomNumber = random()
-        const res = await request(app)
-        .post('/auth/register')
-        .send({username: 'testUser' + randomNumber, password: ''})
-
-        expect(res.statusCode).toBe(400)
-        expect(res.body.error).toBe('Please enter a password')
+        const response = await axios.post(baseUrl + '/auth/register', {
+            username: 'testUser' + randomNumber,
+            password: ''
+        }, {
+            validateStatus: null
+        })
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Please enter a password')
     })
 
-    it('Register - valid username and invalid password (too short)', async () => {
+    it('Register - valid username with invalid password (too short)', async() => {
         const randomNumber = random()
-        const res = await request(app)
-            .post('/auth/register')
-            .send({username: 'testingUser' + randomNumber, password: 'badPass'})
-
-            expect(res.statusCode).toBe(400);
-            expect(res.body.error).toBe('Password must be at least 8 characters long')
+        const response = await axios.post(baseUrl + '/auth/register', {
+            username: 'testUser' + randomNumber,
+            password: 'badPass'
+        },{
+            validateStatus: null
+        })
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Password must be at least 8 characters long')
     })
 
-    it('Register - valid username and invalid password (corrent length but no number)', async () => {
-        const randoNumber = random()
-        const res = await request(app)
-            .post('/auth/register')
-            .send({username:'testingUser' + randoNumber, password:'testingpasswordNoNumber'})
-
-            expect(res.statusCode).toBe(400);
-            expect(res.body.error).toBe('Password must contain at least one number')
+    it('Register - valid username and invalid password (corrent length but no number)', async() => {
+        const randomNumber = random()
+        const response = await axios.post(baseUrl + '/auth/register', {
+            username: 'testUser' + randomNumber,
+            password: 'testingNoNumberPassword'
+        },{
+            validateStatus: null
+        })
+        expect(response.status).toBe(400)
+        expect(response.data.error).toBe('Password must contain at least one number')
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // it('Register - valid username and invalid password (too short)', async () => {
+    //     const randomNumber = random()
+    //     const res = await request(app)
+    //         .post('/auth/register')
+    //         .send({username: 'testingUser' + randomNumber, password: 'badPass'})
+
+    //         expect(res.statusCode).toBe(400);
+    //         expect(res.body.error).toBe('Password must be at least 8 characters long')
+    // })
+
+    // it('Register - valid username and invalid password (corrent length but no number)', async () => {
+    //     const randoNumber = random()
+    //     const res = await request(app)
+    //         .post('/auth/register')
+    //         .send({username:'testingUser' + randoNumber, password:'testingpasswordNoNumber'})
+
+    //         expect(res.statusCode).toBe(400);
+    //         expect(res.body.error).toBe('Password must contain at least one number')
+    // })
 })
